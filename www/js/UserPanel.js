ebcf0708939b6a30c5d5d4407af73d85b7305d1f
ruxinhan <rhan@andrew.cmu.edu>
2013-11-30 11:25:44 +1030

    1. change the display: mentioned 1 times -> mentioned 1 time 2. Changed the count of 12 to an if option

--- www/js/UserPanel.js
+++ www/js/UserPanel.js
@@ -2,45 +2,72 @@ var users;
 var userpic;
 var countUsers;
 var users_length;
+var end_user;
 
 function UserPanel(){
 	users = resultObj.users;
 	users_length = users.length;
 	userpic=[];
 	countUsers=0;
-	for(var i=0; i<12; i++){
+	end_user = (users_length > 12) ? 12 : users_length;
+	for(var i=0; i<end_user; i++){
 		getUserInfo(users[i].text);
 	}
 	
 }
 
 function showUserPanel(){
-	for(var i=0; i<12; i++){
-
-		var userHtml = 
+	for(var i=0; i<end_user; i++){
+		if(users[i].size > 1){
+			var userHtml = 
                 "<div style='margin-bottom:5px; float:left; width:33%; height:150px '>" +
                     "<p style='font-size:8px'>{0}</p>" +
                     "<img src='{1}' height='80px'>" +
                     "<p style='font-size:8px'>(Mentioned {2} times)</p>" +
                 "</div>";
 
-        userHtml = String.format( userHtml,
+        	userHtml = String.format( userHtml,
                 users[i].text,
                 userpic[i],
                 users[i].size);
+		}else if(users[i].size == 1){
+			var userHtml = 
+                "<div style='margin-bottom:5px; float:left; width:33%; height:150px '>" +
+                    "<p style='font-size:8px'>{0}</p>" +
+                    "<img src='{1}' height='80px'>" +
+                    "<p style='font-size:8px'>(Mentioned 1 time)</p>" +
+                "</div>";
+
+        	userHtml = String.format( userHtml,
+                users[i].text,
+                userpic[i]);
+		}
+		
        	$("#mentionedDiv").append(userHtml);
        
 	}
-	for(var i=12; i<users_length; i++){
-		var userHtml = 
+	for(var i=end_user; i<users_length; i++){
+		if(users[i].size > 1){
+			var userHtml = 
                 "<div style='margin-bottom:5px; float:left; width:33% '>" +
                     "<p style='font-size:8px'>{0}</p>" +
-                    "<p style='font-size:8px'>(Mentioned {2} times)</p>" +
+                    "<p style='font-size:8px'>(Mentioned {1} times)</p>" +
                 "</div>";
 
-        userHtml = String.format( userHtml,
+        	userHtml = String.format( userHtml,
                 users[i].text,
                 users[i].size);
+		}else if(users[i].size == 1){
+			var userHtml = 
+                "<div style='margin-bottom:5px; float:left; width:33% '>" +
+                    "<p style='font-size:8px'>{0}</p>" +
+                    "<p style='font-size:8px'>(Mentioned 1 time)</p>" +
+                "</div>";
+
+        	userHtml = String.format( userHtml,
+                users[i].text);
+		}
+		
                 
         $("#mentionedDiv").append(userHtml);
 	}
@@ -59,7 +86,7 @@ function getUserInfo(id){
             src = obj.pic;
 			userpic.push(src);
 			countUsers++;  
-			if (countUsers == 12){ showUserPanel();}       
+			if (countUsers == end_user){ showUserPanel();}       
         })
         .fail(function(jqxhr, textStatus, error) {
             var err = textStatus + ", " + error;
