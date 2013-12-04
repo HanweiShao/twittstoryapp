1d8ea23c2eef0e138a9030cf7f110a81b59b0e81
OrangeSean <haoxin418@hotmail.com>
2013-12-04 19:10:39 +1030

    update videos -> it works now!

--- www/js/videos.js
+++ www/js/videos.js
@@ -15,41 +15,33 @@ function videos(url) {
 	var videoHtml;
 
 	if (m[2] === "www.youtube.com") {
-		fn = YoutubeWidget(url);
+		fn = new youtubeVideo(url);
 		videoHtml = fn.renderOn();
 	} else if (m[2] === "vimeo.com") {
-		fn = VimeoWidget(url);
+		fn = new vimeoVideo(url);
 		videoHtml = fn.renderOn();
 	} else if (m[2] === "vine.co") {
-		fn = VineWidget(url);
+		fn = new vineVideo(url);
 		videoHtml = fn.renderOn();
 	}
-	if (fn === undefined)
-		videoHtml = "<h1>No Videos!</h1>";
-
+	if (fn === undefined) {
+		// nothing
+	}
 	$('#imageDiv').append(videoHtml);
 }
 
-function YoutubeWidget(url) {
-	var _url = url;
-	this.url = function(val) {
-		if (val === undefined) {
-			return _url;
-		} else {
-			_url = val;
-			return this;
-		}
-	};
+function youtubeVideo(url) {
+	this.url = url;
 	this.embedUrl = function() {
 		// Code taken and adapted from:
 		// http://stackoverflow.com/questions/13100611/replace-url-from-youtube-to-embed-code-error-permission-denied-to-access-prop
 		var regex = /(\?v=|\&v=|\/\d\/|\/embed\/|\/v\/|\.be\/)([a-zA-Z0-9\-\_]+)/;
-		var result = this.url().match(regex);
+		var result = this.url.match(regex);
 		if (result) {
 			return "http://www.youtube.com/embed/" + result[2];
 		} else {
 			// Code shouldn't reach here
-			var parts = this.url().split("?v=");
+			var parts = this.url.split("?v=");
 			return "//www.youtube.com/embed/" + parts[parts.length - 1];
 		}
 	};
@@ -58,23 +50,14 @@ function YoutubeWidget(url) {
 		// frameborder="0" allowfullscreen></iframe>
 		youtubeHtml = "<iframe width={0}, heihgt={1} src={2} frameborder={3} allowfullscreen></iframe>";
 		youtubeHtml = String.format(youtubeHtml, uniWidth, uniHeight, this.embedUrl(), "0");
-		return youtebeHtml;
+		return youtubeHtml;
 	};
-};
+}
 
-function VimeoWidget(url) {
-	var _url = url;
-	this.url = function(val) {
-		if (val === undefined) {
-			return _url;
-		} else {
-			_url = val;
-			return this;
-		}
-	};
-
+function vimeoVideo(url) {
+	this.url = url;
 	this.embedUrl = function() {
-		var parts = this.url().split("/");
+		var parts = this.url.split("/");
 		return "http://player.vimeo.com/video/" + parts[parts.length - 1] + "?color=a8a8a8";
 	};
 
@@ -87,22 +70,14 @@ function VimeoWidget(url) {
 	};
 }
 
-function VineWidget(url) {
-	var _url = url;
-	this.url = function(val) {
-		if (val === undefined) {
-			return _url;
-		} else {
-			_url = val;
-			return this;
-		}
-	};
+function vineVideo(url) {
+	this.url = url;
 
 	this.renderOn = function() {
 		// <iframe src="https://vine.co/v/bnrtW52x1uJ/card?mute=1"
 		//  width="500px" height="500px" frameborder="0"></iframe>
 		vineHtml = "<iframe src={0} width={1}, heihgt={2} frameborder={3}></iframe>";
-		vineHtml = String.format(vimeoHtml, this.url() + "/card?mute=1", uniWidth, uniHeight, "0");
+		vineHtml = String.format(vimeoHtml, this.url + "/card?mute=1", uniWidth, uniHeight, "0");
 		return vineHtml;
 	};
 }
